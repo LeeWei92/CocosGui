7ff83069407248093da0aec4d39d9efb6e6c9d4d
liwei002 <liwei002@bianfeng.com>
2016-08-02 11:31:14 +0800

    add function of downloadSavePath(setter and getter) for HttpAsynSprite

--- CocosGui/HttpAsynSprite.cpp
+++ CocosGui/HttpAsynSprite.cpp
@@ -2,7 +2,7 @@
 //  HttpAsynSprite.cpp
 //  GUI
 //  shengxiaainj@gmail.com
-//  Created by Lee on 15/10/22.
+//  Created by LeeWei on 15/10/22.
 //
 //
 
@@ -14,287 +14,299 @@ NS_CC_GUI_BEGIN
 
 size_t writeData(void *ptr, size_t size, size_t nmemb, void *data)
 {
-    size_t realsize = size * nmemb;
-    
-    struct OutputStruct *mem = (struct OutputStruct *) data;
-    
-    mem->buffer = (char*)realloc(mem->buffer, mem->size + realsize + 1);
-    
-    if (mem->buffer)
-    {
-        memcpy( &( mem->buffer[ mem->size ]), ptr, realsize );
-        mem->size += realsize;
-        mem->buffer[ mem->size ] = 0;
-    }
-    return realsize;
+	size_t realsize = size * nmemb;
+
+	struct OutputStruct *mem = (struct OutputStruct *) data;
+
+	mem->buffer = (char*)realloc(mem->buffer, mem->size + realsize + 1);
+
+	if (mem->buffer)
+	{
+		memcpy(&(mem->buffer[mem->size]), ptr, realsize);
+		mem->size += realsize;
+		mem->buffer[mem->size] = 0;
+	}
+	return realsize;
 }
 
 int progressFunc(void *ptr, double totalToDownload, double nowDownloaded, double totalToUpLoad, double nowUpLoaded)
 {
-    static int percent = 0;
-    int tmp = 0;
-    if (totalToDownload > 0)
-    {
-        tmp = (int)(nowDownloaded / totalToDownload * 100);
-    }
-    
-    /*
-    if ((static_cast<AssetsManager*>(ptr))->_cancelDownload)
-    {
-        curl_easy_pause((static_cast<AssetsManager*>(ptr))->_curl, CURLPAUSE_RECV);
-        curl_easy_cleanup((static_cast<AssetsManager*>(ptr))->_curl);
-         fclose((static_cast<AssetsManager*>(ptr))->_fp);
-        return 0;
-    }
-    */
-    
-    if (percent != tmp)
-    {
-        percent = tmp;
-        /*
-        Director::getInstance()->getScheduler()->performFunctionInCocosThread([=]
-        {
-            auto manager = static_cast<AssetsManager*>(ptr);
-            if (manager->_delegate)
-                manager->_delegate->onProgress(percent);
-        });
-        */
-        CCLOG("downloading... %d%%", percent);
-    }
-    
-    return 0;
+	static int percent = 0;
+	int tmp = 0;
+	if (totalToDownload > 0)
+	{
+		tmp = (int)(nowDownloaded / totalToDownload * 100);
+	}
+
+	/*
+	if ((static_cast<HttpAsynSprite*>(ptr))->_cancelDownload)
+	{
+	curl_easy_pause((static_cast<HttpAsynSprite*>(ptr))->_curl, CURLPAUSE_RECV);
+	curl_easy_cleanup((static_cast<HttpAsynSprite*>(ptr))->_curl);
+	fclose((static_cast<HttpAsynSprite*>(ptr))->_fp);
+	return 0;
+	}
+	*/
+
+	if (percent != tmp)
+	{
+		percent = tmp;
+		/*
+		Director::getInstance()->getScheduler()->performFunctionInCocosThread([=]
+		{
+		auto manager = static_cast<HttpAsynSprite*>(ptr);
+		if (manager->_delegate)
+		manager->_delegate->onProgress(percent);
+		});
+		*/
+		CCLOG("downloading... %d%%", percent);
+	}
+
+	return 0;
 }
 
 HttpAsynSprite::HttpAsynSprite()
 : m_bDownloading(false)
 {
-    m_sSavePath = FileUtils::getInstance()->getWritablePath();
+	m_sSavePath = FileUtils::getInstance()->getWritablePath();
 }
 
 HttpAsynSprite::~HttpAsynSprite()
 {
-    unschedule(CC_SCHEDULE_SELECTOR(HttpAsynSprite::update));
+	unschedule(CC_SCHEDULE_SELECTOR(HttpAsynSprite::update));
 }
 
 HttpAsynSprite* HttpAsynSprite::create(const std::string &defauleFileName)
 {
-    auto sprite = new (std::nothrow) HttpAsynSprite();
-    if (sprite && sprite->initWithFile(defauleFileName))
-    {
-        sprite->autorelease();
-        return sprite;
-    }
-    CC_SAFE_DELETE(sprite);
-    return nullptr;
+	auto sprite = new (std::nothrow) HttpAsynSprite();
+	if (sprite && sprite->initWithFile(defauleFileName))
+	{
+		sprite->autorelease();
+		return sprite;
+	}
+	CC_SAFE_DELETE(sprite);
+	return nullptr;
 }
 
 HttpAsynSprite* HttpAsynSprite::create(const std::string &defauleFileName,
-                                       const std::string &url)
+	const std::string &url)
 {
-    auto sprite = new (std::nothrow) HttpAsynSprite();
-    if (sprite && sprite->initWithUrl(defauleFileName, url))
-    {
-        sprite->autorelease();
-        return sprite;
-    }
-    CC_SAFE_DELETE(sprite);
-    return nullptr;
+	auto sprite = new (std::nothrow) HttpAsynSprite();
+	if (sprite && sprite->initWithUrl(defauleFileName, url))
+	{
+		sprite->autorelease();
+		return sprite;
+	}
+	CC_SAFE_DELETE(sprite);
+	return nullptr;
 }
 
 HttpAsynSprite* HttpAsynSprite::createWithURL(const std::string& url)
 {
-    auto sprite = new (std::nothrow) HttpAsynSprite();
-    if (sprite && sprite->initWithUrl(url))
-    {
-        sprite->autorelease();
-        return sprite;
-    }
-    CC_SAFE_DELETE(sprite);
-    return nullptr;
+	auto sprite = new (std::nothrow) HttpAsynSprite();
+	if (sprite && sprite->initWithUrl(url))
+	{
+		sprite->autorelease();
+		return sprite;
+	}
+	CC_SAFE_DELETE(sprite);
+	return nullptr;
 }
 
 bool HttpAsynSprite::initWithFile(const std::string& filename)
 {
-    if (filename.size() <= 0)
-    {
-        return false;
-    }
-    auto frame = SpriteFrameCache::getInstance()->getSpriteFrameByName(filename);
-    if (frame)
-    {
-        return Sprite::initWithSpriteFrameName(filename);
-    } else
-    {
-        return Sprite::initWithFile(filename);
-    }
-    return true;
+	if (filename.size() <= 0)
+	{
+		return false;
+	}
+	auto frame = SpriteFrameCache::getInstance()->getSpriteFrameByName(filename);
+	if (frame)
+	{
+		return Sprite::initWithSpriteFrameName(filename);
+	}
+	else
+	{
+		return Sprite::initWithFile(filename);
+	}
+	return true;
 }
 
 bool HttpAsynSprite::initWithUrl(const std::string &defauleFileName, const std::string &url)
 {
-    if (!initWithFile(defauleFileName))
-    {
-        return false;
-    }
-    
-    m_url = url;
-    
-    startDownload();
-    
-    return true;
+	if (!initWithFile(defauleFileName))
+	{
+		return false;
+	}
+
+	m_url = url;
+
+	startDownload();
+
+	return true;
 }
 
 bool HttpAsynSprite::initWithUrl(const std::string &url)
 {
-    if (!Sprite::init())
-    {
-        return false;
-    }
-    
-    m_url = url;
-    
-    startDownload();
-    
-    return true;
+	if (!Sprite::init())
+	{
+		return false;
+	}
+
+	m_url = url;
+
+	startDownload();
+
+	return true;
 }
 
 void HttpAsynSprite::setUrl(const std::string &url)
 {
-    m_url = url;
+	m_url = url;
+}
+
+void HttpAsynSprite::setSavePath(const std::string& filePath)
+{
+	if (!FileUtils::getInstance()->isDirectoryExist(filePath))
+	{
+		FileUtils::getInstance()->createDirectory(filePath);
+	}
+	m_sSavePath = filePath;
 }
 
 void HttpAsynSprite::startDownload()
 {
-    if (0 >= m_url.length()) return;
+	if (0 >= m_url.length()) return;
 
-    size_t index = m_url.find_last_of('/');
-    std::string fileName;
-    if (0 < index)
-    {
-        fileName = m_url.substr(index + 1, m_url.length());
-    }
-    
-    std::string filePath = m_sSavePath + fileName;
-    Texture2D *texture = Director::getInstance()->getTextureCache()->addImage(filePath);
-    if (texture)
-    {
-        setTexture(filePath);
-    } else
-    {
-        // http start downloading
-        log("startDownload fileName %s", fileName.c_str());
-        m_url = stringKeyFilter(m_url);
-        
-        if (m_bDownloading) return;
-        
-        std::thread(&HttpAsynSprite::download, this, m_url).detach();
-        
-        if(!m_bDownloading)
-        {
-            schedule(CC_SCHEDULE_SELECTOR(HttpAsynSprite::update));
-            m_bDownloading = true;
-        }
-    }
+	size_t index = m_url.find_last_of('/');
+	std::string fileName;
+	if (0 < index)
+	{
+		fileName = m_url.substr(index + 1, m_url.length());
+	}
+
+	std::string filePath = m_sSavePath + fileName;
+	Texture2D *texture = Director::getInstance()->getTextureCache()->addImage(filePath);
+	if (texture)
+	{
+		setTexture(filePath);
+	}
+	else
+	{
+		// http start downloading
+		log("startDownload fileName %s", fileName.c_str());
+		m_url = stringKeyFilter(m_url);
+
+		if (m_bDownloading) return;
+
+		std::thread(&HttpAsynSprite::download, this, m_url).detach();
+
+		if (!m_bDownloading)
+		{
+			schedule(CC_SCHEDULE_SELECTOR(HttpAsynSprite::update));
+			m_bDownloading = true;
+		}
+	}
 }
 
 void HttpAsynSprite::download(const std::string &url)
 {
-    do
-    {
-        if (!receiveImage(url)) break;
-    } while (0);
+	do
+	{
+		if (!receiveImage(url)) break;
+	} while (0);
 }
 
 bool HttpAsynSprite::receiveImage(const std::string& url)
 {
-    CURL *curl;
-    CURLcode res;
-    
-    struct OutputStruct output;
-    output.buffer = NULL;
-    output.size = 0;
-    
-    curl = curl_easy_init();
-    if(curl)
-    {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeData);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&output);
-        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-        /*
-        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, false);
-        curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progressFunc);
-        curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, this);
-         */
-        curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 1);
-        curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 5);
-        
-        res = curl_easy_perform(curl);
-        
-        curl_easy_cleanup(curl);
-        if (res == 0)
-        {
-            m_urlOutput = output;
-            log("*SUCCEED* Get image from url %s", url.c_str());
-            return true;
-        }
-        log("*FAILED* Get image from url %s with Error: %i", url.c_str(), res);
-    }
-    
-    return false;
+	CURL *curl;
+	CURLcode res;
+
+	struct OutputStruct output;
+	output.buffer = NULL;
+	output.size = 0;
+
+	curl = curl_easy_init();
+	if (curl)
+	{
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeData);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&output);
+		curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+		/*
+		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, false);
+		curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progressFunc);
+		curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, this);
+		*/
+		curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 1);
+		curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 5);
+
+		res = curl_easy_perform(curl);
+
+		curl_easy_cleanup(curl);
+		if (res == 0)
+		{
+			m_urlOutput = output;
+			log("*SUCCEED* Get image from url %s", url.c_str());
+			return true;
+		}
+		log("*FAILED* Get image from url %s with Error: %i", url.c_str(), res);
+	}
+
+	return false;
 }
 
 void HttpAsynSprite::update(float dt)
 {
-    if(m_urlOutput.buffer && m_urlOutput.size > 0)
-    {
-        size_t index = m_url.find_last_of('/');
-        std::string fileName;
-        if (0 < index)
-        {
-            fileName = m_url.substr(index + 1, m_url.length());
-        }
-        std::string filePath = m_sSavePath + fileName;
-        FileUtils::getInstance()->removeFile(filePath);
-        FILE *pFile = fopen(filePath.c_str(), "rb");
-        if (!pFile)
-        {
-            std::ofstream fout(filePath.c_str(), std::ios::binary | std::ios::app);
-            for (unsigned int i = 0; i < m_urlOutput.size; ++i)
-            {
-                fout.write(&(m_urlOutput.buffer)[i], sizeof(char));
-            }
-            fout.close();
-            log("%s file download complete!", fileName.c_str());
-            setTexture(filePath);
-        }
-        if (pFile)
-        {
-            fclose(pFile);
-        }
-    
-        m_bDownloading = false;
-        unschedule(CC_SCHEDULE_SELECTOR(HttpAsynSprite::update));
-    }
+	if (m_urlOutput.buffer && m_urlOutput.size > 0)
+	{
+		size_t index = m_url.find_last_of('/');
+		std::string fileName;
+		if (0 < index)
+		{
+			fileName = m_url.substr(index + 1, m_url.length());
+		}
+		std::string filePath = m_sSavePath + fileName;
+		if (FileUtils::getInstance()->isFileExist(filePath))
+			FileUtils::getInstance()->removeFile(filePath);
+		FILE *pFile = fopen(filePath.c_str(), "rb");
+		if (!pFile)
+		{
+			std::ofstream fout(filePath.c_str(), std::ios::binary | std::ios::app);
+			for (unsigned int i = 0; i < m_urlOutput.size; ++i)
+			{
+				fout.write(&(m_urlOutput.buffer)[i], sizeof(char));
+			}
+			fout.close();
+			log("%s file download complete!", fileName.c_str());
+			setTexture(filePath);
+		}
+		if (pFile)
+		{
+			fclose(pFile);
+		}
+
+		m_bDownloading = false;
+		unschedule(CC_SCHEDULE_SELECTOR(HttpAsynSprite::update));
+	}
 }
 
 std::string HttpAsynSprite::stringKeyFilter(const std::string& srcString)
 {
-    std::string result = srcString;
-    for (auto it = result.begin(); it != result.end(); ++it)
-    {
-        const char* key = "/";
-        if (0 == strcmp(&(*it), key))
-        {
-            result.erase(it);
-        }
-    }
-    
-    return result;
+	std::string result = srcString;
+	for (auto it = result.begin(); it != result.end(); ++it)
+	{
+		const char* key = "/";
+		if (0 == strcmp(&(*it), key))
+		{
+			result.erase(it);
+		}
+	}
+
+	return result;
 }
 
 NS_CC_GUI_END
--- CocosGui/HttpAsynSprite.h
+++ CocosGui/HttpAsynSprite.h
@@ -2,14 +2,17 @@
 //  HttpAsynSprite.h
 //  GUI
 //  shengxiaainj@gmail.com
-//  Created by Lee on 15/10/22.
+//  Created by LeeWei on 15/10/22.
 //
 //
 
 #ifndef HttpAsynSprite_h
 #define HttpAsynSprite_h
 
-#include "GuiMacros.h"
+#include "cocos2d.h"
+#include "../macros.h"
+
+USING_NS_CC;
 
 NS_CC_GUI_BEGIN
 
@@ -27,31 +30,33 @@ struct OutputStruct
 class HttpAsynSprite : public Sprite
 {
 public:
-    static HttpAsynSprite* create(const std::string& defauleFileName);
-    static HttpAsynSprite* create(const std::string& defauleFileName, const std::string& url);
-    static HttpAsynSprite* createWithURL(const std::string& url);
-    
-    bool initWithUrl(const std::string& url);
-    bool initWithUrl(const std::string& defauleFileName, const std::string& url);
+	static HttpAsynSprite* create(const std::string& defauleFileName);
+	static HttpAsynSprite* create(const std::string& defauleFileName, const std::string& url);
+	static HttpAsynSprite* createWithURL(const std::string& url);
 
-    CC_SYNTHESIZE_READONLY(std::string, m_url, Url);
-    
-    void setUrl(const std::string& url);
-    void startDownload();
+	bool initWithUrl(const std::string& url);
+	bool initWithUrl(const std::string& defauleFileName, const std::string& url);
+
+	CC_SYNTHESIZE_READONLY(std::string, m_url, Url);
+
+	void setUrl(const std::string& url);
+	void setSavePath(const std::string& filePath);
+	std::string getSavePath() { return m_sSavePath; }
+	void startDownload();
 private:
-    std::string m_sSavePath;
-    OutputStruct m_urlOutput;
-    bool m_bDownloading;
-    
-    HttpAsynSprite();
-    ~HttpAsynSprite();
-    
-    std::string stringKeyFilter(const std::string& srcString);
+	std::string m_sSavePath;
+	OutputStruct m_urlOutput;
+	bool m_bDownloading;
+
+	HttpAsynSprite();
+	~HttpAsynSprite();
+
+	std::string stringKeyFilter(const std::string& srcString);
 protected:
-    bool initWithFile(const std::string& filename);
-    void download(const std::string &url);
-    bool receiveImage(const std::string& url);
-    void update(float dt);
+	bool initWithFile(const std::string& filename);
+	void download(const std::string &url);
+	bool receiveImage(const std::string& url);
+	void update(float dt);
 };
 
 NS_CC_GUI_END
